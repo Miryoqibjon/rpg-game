2aebe30bdc41db79073924caf536a08983736b76
unknown <eba4194@gmail.com>
2024-07-23 00:01:44 +0500

    RPG game with Phaser JS

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import Phaser, { scenes } from "phaser";
+import './style.css'
+
+new Phaser.Game({
+  width: 800,
+  height: 600,
+  title: 'Phaser RPG',
+  scene: scenes,
+  url: import.meta.env.URL || '',
+  version: import.meta.env.VERSION || '0.0.1',
+  backgroundColor: '#000',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  pixelArt: true,
+})
